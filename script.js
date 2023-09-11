ce619fa5c580cf6a95e882b35cef61c243d0861e
Tyler <122329987+ty-hoffman@users.noreply.github.com>
2023-09-11 11:15:10 -0400

    Create initial math functions

---
+++ script.js
@@ -0,0 +1,20 @@
+let num1 = 0;
+let num2 = 0;
+let operation = "";
+
+function add (num1, num2) {
+    let sum = num1 + num2;
+    return sum;
+}
+function subtract (num1, num2) {
+    let difference = num1 - num2;
+    return difference;
+}
+function multiply (num1, num2) {
+    let product = num1 * num2;
+    return product;
+}
+function divide (num1, num2) {
+    let quotient = num1 / num2;
+    return quotient;
+}
